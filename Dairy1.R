88b6a5ef6e1a409d5a8768e8dca106241f3e4b4e
witusj <wfth69@gmail.com>
2013-10-24 22:35:19 +0200

    First file
    A file that grabs dairy price information from www.prodzuivel.nl and
    saves it as csv data (Excel).

---
+++ Dairy1.R
@@ -0,0 +1,46 @@
+library("RCurl")
+library("XML")
+
+setwd("C:/Users/NB/Google Drive/Data Analysis/Dairy")
+
+# Function to grab data from internet site according to given year
+getDairyData <- function(year) {
+  
+  # Build url string
+  urlStr <- paste0("http://www.prodzuivel.nl/pz/noteringen/commissie_historie_",year,".htm")
+  
+  # Get annual table
+  y2013<-getURL(urlStr, ssl.verifypeer = FALSE)
+  
+  # Parse HTML into data frame and add variable names
+  doc <- htmlParse(y2013)
+  tableNodes <- getNodeSet(doc, "//table")
+  tb <- readHTMLTable(tableNodes[[2]], header = c("Datum", "Boter", "Vol melkpoeder", "Mager melkpoeder mager - food", "Mager melkpoeder - feed", "Weipoeder"), skip.rows = 2)
+  tb <- tb[-1,]                        # Delete first row
+  tb <- tb[-nrow(tb)+1:-nrow(tb),]     # Delete last two rows
+  
+    tb[[1]] <- paste0(tb[[1]],"/",year) # Add year to date 
+  
+  tb
+}
+
+# Grab data from internet site and build data frame
+period <- c(2009:2013)
+data<-c()
+for(y in period) {
+  ydata <- getDairyData(y)
+  data <- rbind(data, ydata)
+}
+
+# Change comma's into dots and change class of prices into numeric
+for (i in c(2:6)){
+  data[[i]] <- as.numeric(sub(",", ".",data[[i]]))
+}
+
+# Build file name from last date in file and save as csv
+ldate <- data[[nrow(data), 1]]
+filestring <- paste0("weekly_dairy_prices_ending_", ldate,".csv")
+filestring <- gsub("/","", filestring)
+write.csv(data, file = filestring)
+
+tail(data)
